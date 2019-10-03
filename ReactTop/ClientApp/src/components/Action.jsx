0699d10d110fb339839a0b62f18f47c2dec4cb53
yoshisman8 <4214215+yoshisman8@users.noreply.github.com>
2019-10-02 20:48:40 -0400

    [screams in react]

---
+++ ReactTop/ClientApp/src/components/Action.jsx
@@ -0,0 +1,39 @@
+﻿import React, { Component } from 'react';
+import { Container, Row, ButtonGroup, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
+export default class Action extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: props.id
+		};
+	}
+	render() {
+		return (
+			<div>
+				<Form>
+					<FormGroup>
+						<Label for="name">Action Name </Label>
+						<Input id="name" name="name" type="text" />
+					</FormGroup>
+					<FormGroup>
+						<Label for="description">Description</Label>
+						<textarea style="overflow:auto;resize:none" rows="6" id="description" name="description" className="form-control"> </textarea>
+					</FormGroup>
+					<FormGroup>
+						<Label for="economy">Action Economy</Label>
+						<Input type="select" name="economy" id="economy" multiple>
+							<option> Simple Action </option>
+							<option> Complex Action </option>
+							<option> Reaction </option>
+							<option> Free Action </option>
+						</Input>
+					</FormGroup>
+					<ButtonGroup>
+						<Button onClick={this.props.onAdd(this.props.id)} color="success"> Add </Button>
+					</ButtonGroup>
+				</Form>
+			</div>
+		);
+	}
+}
---
+++ ReactTop/ClientApp/src/components/Skill.jsx
@@ -0,0 +1,49 @@
+﻿import React, { Component } from 'react';
+import Action from './Action';
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
+export default class Skill extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			actions: []
+		};
+	}
+	render() {
+		return (
+			<Container className="rounded border bg-dark">
+				<form>
+					<FormGroup>
+						<Label for="name">Skill Name</Label>
+						<Input type="text" id="name" name="name"/>
+					</FormGroup>
+					<FormGroup>
+						<Label for="flavor">Skill Description</Label>
+						<textarea style="overflow:auto;resize:none" rows="6" id="flavor" name="flavor" className="form-control"> </textarea>
+					</FormGroup>
+					<FormGroup>
+						<Label for="score">Ability Score</Label>
+						<Input type="select" name="score" id="score" multiple>
+							<option> Strength </option>
+							<option> Dexterity </option>
+							<option> Agility </option>
+							<option> Constitution </option>
+							<option> Memory </option>
+							<option> Intuition </option>
+							<option> Charisma </option>
+						</Input>
+
+						<ul>
+							{this.state.actions.map(action => (
+								<li> {action.name} {badgeEcon(action.economy)} <Button close/> </li>
+							))}
+						</ul>
+
+						<Action onAdd={this.handleAdd}/>
+					</FormGroup>
+					<Button> Create </Button>
+				</form>
+			</Container>
+		);
+	}
+}
